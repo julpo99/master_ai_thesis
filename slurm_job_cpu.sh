eb36869c62bace67b30994020ace1bd1be2c6a4f
julpo99 <julpo99@icloud.com>
2025-06-20 16:21:49 +0200

    ADD: slurm_job_cpu.sh only for CPU

---
+++ slurm_job_cpu.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#SBATCH --job-name=lgcn               # Job name
+#SBATCH --time=24:00:00               # Time limit (hh:mm:ss)
+#SBATCH -N 1                          # Number of nodes
+#SBATCH --partition=defq              # Default partition
+#SBATCH --ntasks-per-node=1           # Number of tasks per node
+#SBATCH --cpus-per-task=24           # You can adjust based on expected load
+#SBATCH --constraint=cpunode         # Select only CPU nodes
+#SBATCH --output=output_%j.log        # Output log file (%j will be replaced with the job ID)
+
+
+# Paths
+SCRATCH=/var/scratch/jra223
+SCRIPT_DIR=$SCRATCH/master_ai_thesis
+TRAIN_PATH=$SCRIPT_DIR/"experiments/train.py"
+
+
+# Load required modules for DAS6
+ . /etc/bashrc
+ . /etc/profile.d/lmod.sh
+#module load cuda12.6/toolkit
+#module load cuDNN/cuda12.6
+
+# This loads the anaconda virtual environment with our packages
+source $HOME/.bashrc
+conda activate /var/scratch/jra223/master_ai_thesis/env
+
+
+# Check which Python is active
+#which python
+#python --version
+
+#echo "Running on node: $(hostname)"
+#echo "Using GPU cores: $SLURM_NTASKS"
+
+# Base directory for the experiment (only if not exists)
+if [ ! -d $SCRIPT_DIR/outputs ]; then
+  mkdir $SCRIPT_DIR/outputs
+fi
+# cd if the directory exists
+if [ -d $SCRIPT_DIR/outputs ]; then
+  cd $SCRIPT_DIR/outputs
+else
+  echo "Directory $SCRIPT_DIR/outputs does not exist."
+  exit 1
+fi
+
+# Simple trick to create a unique directory for each run of the script
+echo $$
+mkdir o`echo $$`
+cd o`echo $$`
+
+# Enable memory fragmentation workaround
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export PYTHONPATH=$SCRIPT_DIR
+
+#python <<EOF
+#import torch
+#print(torch.cuda.is_available())
+#print(torch.cuda.get_device_name(0))
+#EOF
+
+
+# Run the actual experiment
+python -u $TRAIN_PATH > 'output.out'
