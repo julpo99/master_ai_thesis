f0a88a1b9dc701d04c22101eede3b2200a365c2e
julpo99 <julpo99@icloud.com>
2025-07-15 16:42:48 +0200

    EDIT: default_config.py

--- configs/default_config.py
+++ configs/default_config.py
@@ -7,8 +7,8 @@ DEFAULT_CONFIG = {
         'epochs': 50,
         'optimizer': 'adam',
         'emb_dim': 16,
-        'bases': 40,
-        'enrich_flag': True
+        'bases': None,
+        'enrich_flag': False
     },
     'rgcn_emb': {
         'model_name': 'rgcn_emb',
@@ -27,25 +27,25 @@ DEFAULT_CONFIG = {
         'lr': 0.001,
         'wd': 0.0005,
         'l2': 0.0,
-        'epochs': 300,
+        'epochs': 50,
         'optimizer': 'adamw',
-        'emb_dim': 256,
-        'rp': 32,
-        'ldepth': 2,
+        'emb_dim': 16,
+        'rp': 33,
+        'ldepth': 0,
         'lwidth': 128,
         'dropout': 0.0,
-        'enrich_flag': True
+        'enrich_flag': False
     },
     'lgcn_rel_emb': {
         'model_name': 'lgcn_rel_emb',
         'lr': 0.01,
-        'wd': 0.1,
-        'l2': 0.001,
-        'epochs': 400,
-        'optimizer': 'adamw',
-        'emb_dim': 64,
-        'rp': 2,
-        'dropout': 0.2,
-        'enrich_flag': True
+        'wd': 0.0,
+        'l2': 0.0005,
+        'epochs': 50,
+        'optimizer': 'adam',
+        'emb_dim': 16,
+        'rp': 33,
+        'dropout': 0.0,
+        'enrich_flag': False
     },
 }
